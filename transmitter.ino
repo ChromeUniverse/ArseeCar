5782b44af31e934c2fe136c0e9f1e208025b6679
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2019-06-07 17:59:55 -0300

    Create transmitter.ino

---
+++ transmitter.ino
@@ -0,0 +1,36 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
+int joyPinX = A0;
+int joyPinY = A1;
+int valueX = 0;
+int valueY = 0;
+
+int treatValue(int data) {
+  return data;
+}
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";
+void setup() { 
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {  
+  valueX = analogRead(joyPinX);  
+  radio.write(&valueX, sizeof(int));
+  delay(50);
+}

39bf39069cdc7253874c7ce4bd7ad48b783186dd
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2019-06-09 01:38:46 -0300

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,11 +1,5 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
+
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
 
 int joyPinX = A0;
 int joyPinY = A1;

bc879f767be0503c7e0b1239090ba713cc1439b5
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2019-07-05 15:34:47 -0300

    Did some stuff

--- hobo.ino
+++ hobo.ino
@@ -1,17 +1,22 @@
+int baseSpeed = 70;
+int blackThreshold = 400;
+int defaultDelay = 50;
+double kp = 0.5;
+
 // Back Left
 #define BLP 45
-#define BL1 35
-#define BL2 37
+#define BL1 37
+#define BL2 35
 
 // Front Left
 #define FLP 44
-#define FL1 41
-#define FL2 39
+#define FL1 39
+#define FL2 41
 
 // Back Right
 #define BRP 4
-#define BR1 34
-#define BR2 36
+#define BR1 36
+#define BR2 34
 
 // Front Right
 #define FRP 46
@@ -19,18 +24,18 @@
 #define FR2 38
 
 // Infrared
+#define IRL A7
+#define IRC A6
 #define IRR A5
-#define IRL A6
-#define IRC A7
 
-// CSL
+// Color Sensor Left
 #define CSL0 22
 #define CSL1 23
 #define CSL2 9
 #define CSL3 8
 #define CSLO 10
 
-// CSR
+// Color Sensor Right
 #define CSR0 52
 #define CSR1 53
 #define CSR2 5
@@ -56,6 +61,13 @@ void setup() {
     pinMode(BL1, OUTPUT);
     pinMode(BL2, OUTPUT);
 
+    pinMode(IRL, INPUT);
+    pinMode(IRC, INPUT);
+    pinMode(IRR, INPUT);
+
+    Serial.begin(9600);
+
+/* COLOR SENSORS
     pinMode(CSL0, OUTPUT);
     pinMode(CSL1, OUTPUT);
     pinMode(CSL2, OUTPUT);
@@ -67,6 +79,7 @@ void setup() {
     pinMode(CSR2, OUTPUT);
     pinMode(CSR3, OUTPUT);
     pinMode(CSRO, INPUT);
+*/
 }
 
 int FRS = 255;
@@ -74,18 +87,100 @@ int FLS = 255;
 int BRS = 255;
 int BLS = 255;
 
+int valIRL = 0;
+int valIRR = 0;
+int valIRC = 0;
+
+void updateIR() {
+    valIRL = analogRead(IRL);
+    valIRC = analogRead(IRC);
+    valIRR = analogRead(IRR);
+
+    Serial.print("Updated IR sensors (L - C - R): ");
+    Serial.print(IRL);
+    Serial.print(" - ");
+    Serial.print(IRC);
+    Serial.println(" - " );
+}
+
+/*
+    LEFT = -1
+    AHEAD = 0
+    RIGHT = +1
+*/
+
+void setMotorDirection(int direction) {
+
+    // All Ahead
+    digitalWrite(FR1, HIGH);
+    digitalWrite(FR2, LOW);
+    digitalWrite(FL1, HIGH);
+    digitalWrite(FL2, LOW);
+    digitalWrite(BR1, HIGH);
+    digitalWrite(BR2, LOW);
+    digitalWrite(BL1, HIGH);
+    digitalWrite(BL2, LOW);
+
+    if (direction == -1) {
+
+        // Revert Left
+        digitalWrite(FL1, LOW);
+        digitalWrite(FL2, HIGH);
+        digitalWrite(BL1, LOW);
+        digitalWrite(BL2, HIGH);
+    } else if (direction == 1) {
+
+        // Revert Right
+        digitalWrite(FR1, LOW);
+        digitalWrite(FR2, HIGH);
+        digitalWrite(BR1, LOW);
+        digitalWrite(BR2, HIGH);
+    }
+}
+
+void setMotorSpeed(int motorID, int motorSpeed) {
+    if (motorSpeed < 0) motorSpeed = 0;
+    if (motorSpeed > 255) motorSpeed = 255;
+
+    analogWrite(motorID, motorSpeed);
+}
+
+void setAllMotorSpeed(int BLS, int BRS, int FLS, int FRS) {
+    setMotorSpeed(BLP, BLS);
+    setMotorSpeed(BRP, BRS);
+    setMotorSpeed(BLP, FLS);
+    setMotorSpeed(BLP, FRS);
+}
+
+bool isOnTrack() {
+    if (valIRC >= blackThreshold) return true;
+    return false; 
+}
+
+int getError() { return IRL-IRR; }
+
+void move(int turnDirection, int motorSpeed, int moveDelay=defaultDelay) {
+
+/*
+    if (turnDirection < 0) setMotorDirection(-1);
+    else if (turnDirection == 0) setMotorDirection(0);
+    else (turnDirection > 0) setMotorDirection(1);
+*/
+
+    int rightSpeed = motorSpeed - kp*turnDirection;
+    int leftSpeed = motorSpeed + kp*turnDirection;
+
+    setMotorSpeed(leftSpeed, rightSpeed, leftSpeed, rightSpeed);
+
+    delay(moveDelay);
+}
+
+void followLine() {
+    int turnDirection = -getError();
+    move(turnDirection, turnSpeed);
+}
+
 void loop() {
-  digitalWrite(FR1, HIGH);
-  digitalWrite(FR2, LOW);
-  digitalWrite(FL1, HIGH);
-  digitalWrite(FL2, LOW);
-  digitalWrite(BR1, HIGH);
-  digitalWrite(BR2, LOW);
-  digitalWrite(BL1, HIGH);
-  digitalWrite(BL2, LOW);
- 
-  analogWrite(FRP, FRS);
-  analogWrite(FLP, FLS);
-  analogWrite(BRP, BRS);
-  analogWrite(BLP, BLS);
+    updateIR();    
+    followLine();
 }
